c30fa223076cd709ef36f87851b311ca5771153c
David Paul <DavidPaul31@gmail.com>
2017-02-19 23:54:12 -0600

    Fix lots of syntax errors
    The validation script is actually executable and functional now.
    A few test cases were added. Some test cases are believed to be
    redundant, but have yet to be proved as such and have therefore
    been left in for now.

--- validate-values.sh
+++ validate-values.sh
@@ -19,7 +19,7 @@
 
 # Collect necessary information via script options
 
-while getopts 'n:b:t:s:' OPTION
+while getopts ':n:b:t:s:' OPTION
 do
     case $OPTION in
 # If someone has strong opinions about what letters the options should
@@ -33,49 +33,95 @@ do
            ;;
         s) sector_size="$OPTARG"
            ;;
-        \?) echo "Invalid option: -$OPTARG" >&2
+        ?) echo "Invalid option: -$OPTARG" >&2
            exit 1
            ;;
     esac
 done
 shift $(( $OPTIND - 1 ))
 
+
+
 #  Input validation
+
+## Verifiy all neccessary options exist
+if [ ! $number_of_disks ];
+then
+    error_insufficient_options="true"
+fi
+
+if [ ! $smallest_disk_size ];
+then
+    error_insufficient_options="true"
+fi
+
+if [ ! $stripe_size ];
+then
+    error_insufficient_options="true"
+fi
+
+if [ ! $sector_size ];
+then
+    error_insufficient_options="true"
+fi
+
+
+
+## Exit with error if insufficient options were passed in
+if [ $error_insufficient_options ];
+then
+#    TODO Display a help page (maybe just actually implement proper exit codes
+#         and have the calling program decide what to do)
+    echo "A required option was never declared" >&2
+    exit 1;
+fi
+
+
+
 ## Verify disk size is a multiple of sector size
-if (( "$smallest_disk_size" % "$sector_size" != 0 ));
+if [ $(($smallest_disk_size % $sector_size)) != 0 ];
+then
+    echo "Disk size must be a multiple of sector size" >&2
+    exit 1;
+fi
+
+## TODO Determine if this check is redundant
+## Verify sector size is smaller than disk size
+if [ "$sector_size" -gt "$smallest_disk_size" ];
 then
     echo "Sector size must be smaller than disk size" >&2
     exit 1;
 fi
-#
+
 ## Verify stripe size is a multiple of the number of disks
-if (( "$stripe_size" % "$number_of_disks" != 0 ));
+if [ $(($stripe_size % $number_of_disks)) != 0 ];
 then
     echo "Stripe size is the combined size of one stripe spanning across all disks" >&2
     exit 1;
 fi
-#
+
 ## Verify stripe size is a multiple of the sector size
-if (( "$stripe_size" % "$sector_size" != 0 ));
+if [ $(($stripe_size % $sector_size)) != 0 ];
 then
     echo "Stripe size must be a multiple of sector size" >&2
     exit 1;
 fi
-#
+
 ## This condition can only be achieved using negative numbers or zero (which are
 ## already accounted for) or by using a combination of positive numbers that would
 ## fail one of the other tests. (I think. Someone please check me on that.)
-if (( "$sector_size" > ("$stripe_size" / "$number_of_disks") ));
+if [ $(($sector_size * $number_of_disks)) -gt "$stripe_size" ];
 then
     echo "Stripe size can't be smaller than sector size times the number of disks" >&2
     exit 1;
 fi
-#
+
 ## Verify stripe size / number of disks is not greater than the smallest disk size
-if (( ("$stripe_size" / "$number_of_disks") > "$smallest_disk_size" ));
+if [ "$stripe_size" -gt $(($number_of_disks * $smallest_disk_size)) ];
 then
     echo "Stripe size too big or smallest disk too small" >&2
     exit 1;
 fi
+
 echo "Input options are valid."
 exit 0
